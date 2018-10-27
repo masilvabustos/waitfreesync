333b32c392a5ef424606e0fad6bd2b602f6ad315
Matías Silva Bustos <matiasasb@gmail.com>
2018-10-27 12:07:07 -0300

    Change 'Size_Full' -> 'Unlimited'

--- src/universal_consensus_traits.ads
+++ src/universal_consensus_traits.ads
@@ -1,7 +1,7 @@
 package Universal_Consensus_Traits is
 
    type Policy is (GENERAL, LIFO);
-   type Cell_Size is (Size_24, Size_Full);
+   type Cell_Size is (Size_24, Unlimited);
    for Cell_Size use (24, 2**32-1);
 
 
