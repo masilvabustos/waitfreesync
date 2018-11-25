543963379ce667515fcdb8a2efccf6ae486fd7f5
Matías Silva Bustos <matiasasb@gmail.com>
2018-11-25 19:25:32 -0300

    Rename LIFO => PRIORIZED

--- src/universal_consensus_traits.ads
+++ src/universal_consensus_traits.ads
@@ -1,6 +1,6 @@
 package Universal_Consensus_Traits is
 
-   type Policy is (GENERAL, LIFO);
+   type Policy is (GENERAL, PRIORIZED);
    type Cell_Size is (Size_24, Unlimited);
 
 end Universal_Consensus_Traits;
